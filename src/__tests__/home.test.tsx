4945b2d66b61d333a73ad35a1a4a5358a3d63370
soljahd <165214530+soljahd@users.noreply.github.com>
2025-05-05 19:09:40 +0300

    feat: configure jest and add basic tests (#5)

---
+++ src/__tests__/home.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/page';
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      level: 1,
+      name: 'Hello it is Next-Level Store!',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
