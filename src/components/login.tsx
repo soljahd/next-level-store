bef725967c044a2e8b1282e93c296d3222ee87e5
soljah <moskuitoz@gmail.com>
2025-05-06 23:52:39 +0300

    init: start ecommerce-application sprint 2

---
+++ src/components/login.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from '@mui/material';
+
+export default function Login() {
+  return (
+    <Container component="main" maxWidth="sm">
+      <Typography component="h1" variant="h4">
+        Login
+      </Typography>
+    </Container>
+  );
+}
