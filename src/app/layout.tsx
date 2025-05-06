491beecaa7d3b780a4c148886427066e05bec617
soljahd <165214530+soljahd@users.noreply.github.com>
2025-05-06 19:07:17 +0300

    feat: integrate of MUI components library (#9)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+import theme from '../theme';
 import type { Metadata } from 'next';
 import './globals.css';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 export const metadata: Metadata = {
   title: 'Next-Level Store',
@@ -11,8 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
