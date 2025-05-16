c1a3b53e404655aef2b731e3c60e46a48c88ba4d
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-05-17 01:25:30 +0300

    feat: implement header drop down menu (#30)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,56 @@
 'use client';
 
-import { AppBar, Toolbar, Button, Box, TextField, InputAdornment } from '@mui/material';
-import { Menu as MenuIcon, HelpOutline, AccountCircle, ShoppingCart, Search } from '@mui/icons-material';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  TextField,
+  InputAdornment,
+  MenuItem,
+  Typography,
+  Menu,
+  styled,
+} from '@mui/material';
+import {
+  Menu as MenuIcon,
+  HelpOutline,
+  AccountCircle,
+  ShoppingCart,
+  Search,
+  Person,
+  ExitToApp,
+  HowToReg,
+  AppRegistration,
+} from '@mui/icons-material';
 import Link from 'next/link';
 import IconButtonLink from '@/components/icon-button-link';
+import MenuLink from './menu-link';
+import { useState } from 'react';
+import { useAuthStore } from '@/lib/store/auth-store';
+
+const CustomMenuItem = styled(MenuItem)({
+  '&:hover': {
+    backgroundColor: 'inherit',
+  },
+  '&.Mui-active': {
+    color: 'inherit',
+  },
+  '& .MuiMenuItem-root.Mui-selected': {
+    backgroundColor: 'inherit',
+  },
+});
 
 export default function Header() {
+  const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorElement);
+  const handleOpenMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElement(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorElement(null);
+  };
+  const { isLoggedIn, setLogoutState } = useAuthStore();
   return (
     <AppBar elevation={0} color="transparent" position="static">
       <Toolbar
@@ -56,7 +101,73 @@ export default function Header() {
         <Box display="flex" sx={{ gap: { xs: 1, md: 2 } }}>
           <IconButtonLink href="/about" icon={<HelpOutline sx={{ width: 28, height: 28 }} />} text="Authors" />
 
-          <IconButtonLink href="/login" icon={<AccountCircle sx={{ width: 28, height: 28 }} />} text="Sign In/Up" />
+          <div>
+            <IconButtonLink
+              onClick={handleOpenMenu}
+              icon={<AccountCircle sx={{ width: 28, height: 28 }} />}
+              text="Account"
+            ></IconButtonLink>
+            <Menu
+              component={Box}
+              anchorEl={anchorElement}
+              open={open}
+              onClose={handleCloseMenu}
+              disableScrollLock
+              anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
+              transformOrigin={{ horizontal: 'center', vertical: 'top' }}
+              sx={{
+                '& .MuiMenuItem-root': {
+                  pl: 1,
+                  pr: 1,
+                  cursor: 'default',
+                },
+              }}
+            >
+              {isLoggedIn
+                ? [
+                    <CustomMenuItem key="profile" disableRipple>
+                      <MenuLink href="/profile" handler={handleCloseMenu}>
+                        <>
+                          <Person />
+                          <Typography>My profile</Typography>
+                        </>
+                      </MenuLink>
+                    </CustomMenuItem>,
+                    <CustomMenuItem key="signOut" disableRipple>
+                      <MenuLink
+                        href="/main"
+                        handler={() => {
+                          setLogoutState();
+                          handleCloseMenu();
+                        }}
+                      >
+                        <>
+                          <ExitToApp />
+                          <Typography>Sign Out</Typography>
+                        </>
+                      </MenuLink>
+                    </CustomMenuItem>,
+                  ]
+                : [
+                    <CustomMenuItem key="signIn" disableRipple>
+                      <MenuLink href="/login" handler={handleCloseMenu}>
+                        <>
+                          <HowToReg />
+                          <Typography>Sign In</Typography>
+                        </>
+                      </MenuLink>
+                    </CustomMenuItem>,
+                    <CustomMenuItem key="signUp" disableRipple>
+                      <MenuLink href="/register" handler={handleCloseMenu}>
+                        <>
+                          <AppRegistration />
+                          <Typography>Sign Up</Typography>
+                        </>
+                      </MenuLink>
+                    </CustomMenuItem>,
+                  ]}
+            </Menu>
+          </div>
 
           <IconButtonLink href="/cart" icon={<ShoppingCart sx={{ width: 28, height: 28 }} />} text="Cart" />
         </Box>
