ad0b4e259c5e5044cdbd4fe893dfced1bd3e45bd
soljah <moskuitoz@gmail.com>
2025-05-17 07:41:45 +0300

    feat: route redirection implemented

--- src/lib/store/auth-store.ts
+++ src/lib/store/auth-store.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import Cookies from 'js-cookie';
 
 type AuthState = {
   isLoggedIn: boolean;
@@ -8,16 +9,41 @@ type AuthState = {
   setLogoutState: () => void;
 };
 
+const authStateStorage = {
+  getItem: (name: string) => {
+    const localValue = Cookies.get(name);
+    if (localValue) {
+      const parsed: unknown = JSON.parse(localValue);
+      return JSON.stringify(parsed);
+    } else {
+      return null;
+    }
+  },
+  setItem: (name: string, value: string) => {
+    Cookies.set(name, value, {
+      path: '/',
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+    });
+  },
+  removeItem: (name: string) => {
+    Cookies.remove(name);
+  },
+};
+
 export const useAuthStore = create(
   persist<AuthState>(
     (set) => ({
       isLoggedIn: false,
       user: null,
       setLoginState: (email) => set({ isLoggedIn: true, user: { email } }),
-      setLogoutState: () => set({ isLoggedIn: false, user: null }),
+      setLogoutState: () => {
+        authStateStorage.removeItem('auth-storage');
+      },
     }),
     {
       name: 'auth-storage',
+      storage: createJSONStorage(() => authStateStorage),
     },
   ),
 );
