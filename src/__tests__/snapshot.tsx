242bbb12836849e11786650e4824770b1eabf102
soljahd <165214530+soljahd@users.noreply.github.com>
2025-05-18 14:29:21 +0300

    feat: jest tests implemented (#37)
    * feat: route redirection implemented
    
    * feat: permanent state implemented
    
    * feat: footer visual updated and favicon updated
    
    * feat: add tests

--- src/__tests__/snapshot.tsx
+++ src/__tests__/snapshot.tsx
@@ -1,7 +1,15 @@
 import { render } from '@testing-library/react';
-import Home from '@/app/page';
+import Home from '@/app/main/page';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 it('renders homepage unchanged', () => {
-  const { container } = render(<Home />);
+  const { container } = render(
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>,
+  );
   expect(container).toMatchSnapshot();
 });
